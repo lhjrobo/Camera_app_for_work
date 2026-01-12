0005b719557dba69220b217417cd79f3146b2a32
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2026-01-12 22:41:48 +0900

    v1_stable

--- ios/WorkCameraApp/AppDelegate.swift
+++ ios/WorkCameraApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "WorkCameraApp",
+      withModuleName: "WorkCameraApp_v2",
       in: window,
       launchOptions: launchOptions
     )
